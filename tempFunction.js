66cf21b67ddadc66437adf580f5f01c9de650b2d
Fay Hsu <fayxujob@gmail.com>
2013-02-06 17:34:22 +0800

    add foo bar

--- tempFunction.js
+++ tempFunction.js
@@ -1,5 +1,13 @@
 var stringJoin = function() {
     return Array.prototype.join(arguments, '');
-}
+};
 
 var o = {};
+
+var foo = function() {
+    console.log('foo');
+};
+
+var bar = function() {
+    console.log('bar');
+};
